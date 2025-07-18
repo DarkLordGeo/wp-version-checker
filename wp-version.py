25ef8d465db4d2dbd409fd41a17c99bc42156b2e
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-07-18 23:35:56 +0400

    fixed

--- wp-version.py
+++ wp-version.py
@@ -1,12 +1,18 @@
 import requests
 userInput = input("Enter wordpress website: ")
-if userInput:
+if userInput != "":
     text = requests.get(f"{userInput.replace(" ","")}/feed")
     generator = text.text
     splitText = generator.split()
+
     genStart = generator.find("<generator>")
     genEnd = generator.find("</generator>")
-    version = generator[genStart:genEnd]
-    print(version[version.find("v=")+ 2:])
 
+    if genStart == -1 and genEnd == -1:
+        print("Given website does not seems to be powered by Wordpress")
+    else:
+        version = generator[genStart:genEnd]
+        print(version[version.find("v=")+ 2:])
 
+else:
+    print("Invalid input")
