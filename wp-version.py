f5f545bea9dc9265d9b9e1ab435c4989203d5d3c
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-07-18 22:39:26 +0400

    simple python script

---
+++ wp-version.py
@@ -0,0 +1,14 @@
+import requests
+import re
+userInput = input("Enter wordpress website: ")
+if userInput:
+    text = requests.get(f"{userInput.replace(" ","")}/feed")
+    generator = text.text
+    generatorSrc = re.findall("<generator>",generator)
+    splitText = generator.split()
+    genStart = generator.find("<generator>")
+    genEnd = generator.find("</generator>")
+    version = generator[genStart:genEnd]
+    print(version[version.find("v=")+ 2:])
+
+
